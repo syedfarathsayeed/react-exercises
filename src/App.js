369c5188bc4806bdb7d25e8400a41ecb8b39886f
Syed Farath Sayeed <syedfarathsayeed@MacBook-Pro-de-Syed.local>
2020-10-07 16:28:47 +0200

    updated subsription card exercise

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { NumberGenerator } from "./exercises"
+import { SubscribeCard } from "./exercises"
 
 function App() {
-  return <NumberGenerator />
+  return <SubscribeCard />
 }
 
 export default App;
--- src/exercises/SubscribeCard/SubscribeCard.js
+++ src/exercises/SubscribeCard/SubscribeCard.js
@@ -1,18 +1,23 @@
 import React from "react"
+import { Header } from "../../componentsUtils"
 import "./SubscribeCard.scss"
 
 function SubscribeCard() {
     return (
-        <div className="card">
-            <h1>SUBSCRIBE</h1>
-            <h5>Sign up with your email address to receive news and updates.</h5>
-            <div>
-                <input placeholder="First name" />
-                <input placeholder="Last name" />
-                <input placeholder="Email" />
+        <>
+            <Header title="Subscription card"
+                description="In this exercise, a subscription card as shown below is developed" />
+            <div className="card">
+                <h1>SUBSCRIBE</h1>
+                <h5>Sign up with your email address to receive news and updates.</h5>
+                <div>
+                    <input placeholder="First name" />
+                    <input placeholder="Last name" />
+                    <input placeholder="Email" />
+                </div>
+                <button>Subscribe</button>
             </div>
-            <button>Subscribe</button>
-        </div>
+        </>
     )
 }
 
